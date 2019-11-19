c183bc4a7546c9079c0888cfdc259ec5fc569588
Alan Bergland <aob2x@virginia.edu>
2019-11-19 14:52:50 -0500

    Update getSNP_list_drosRTEC.sh

--- add_DGN_data/getSNP_list_drosRTEC.sh
+++ add_DGN_data/getSNP_list_drosRTEC.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
 #SBATCH -t 1:00:00 ### 1 hours
-#SBATCH --mem 1G
+#SBATCH --mem 8G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/getSNP_list_drosRTEC.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/getSNP_list_drosRTEC.%A_%a.err # Standard error
 #SBATCH -p standard
