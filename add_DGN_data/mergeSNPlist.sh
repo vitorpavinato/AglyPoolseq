2c8ead04be0412131e8d70653efac98a40f069de
Alan Bergland <aob2x@virginia.edu>
2020-02-15 12:34:36 -0500

    Update mergeSNPlist.sh

--- add_DGN_data/mergeSNPlist.sh
+++ add_DGN_data/mergeSNPlist.sh
@@ -22,10 +22,18 @@ module load gcc/7.1.0 bedops/2.4.1
 # --tmpdir /scratch/aob2x/tmp - > /scratch/aob2x/dest/dest/dgn_drosRTEC_drosEU.sites.dm6.bed
 
 
+#bedops -u \
+#/scratch/aob2x/dest/dgn/sitesData/dgn_sites.dm6.bed \
+#/scratch/aob2x/dest/drosRTEC/drosRTEC_sites.dm6.bed \
+#/scratch/aob2x/dest/drosEU/drosEU_sites.dm6.bed |  \
+#grep -E "chr2L[[:space:]]|chr2R[[:space:]]|chr3L[[:space:]]|chr3R[[:space:]]|chrX[[:space:]]" | \
+#sort-bed  --max-mem 8G \
+#--tmpdir /scratch/aob2x/tmp - > /scratch/aob2x/dest/dest/dgn_drosRTEC_drosEU.sites.noMerge.dm6.bed
+
 bedops -u \
 /scratch/aob2x/dest/dgn/sitesData/dgn_sites.dm6.bed \
 /scratch/aob2x/dest/drosRTEC/drosRTEC_sites.dm6.bed \
 /scratch/aob2x/dest/drosEU/drosEU_sites.dm6.bed |  \
-grep -E "chr2L[[:space:]]|chr2R[[:space:]]|chr3L[[:space:]]|chr3R[[:space:]]|chrX[[:space:]]" | \
+grep -E "chr2L[[:space:]]|chr2R[[:space:]]|chr3L[[:space:]]|chr3R[[:space:]]|chrX[[:space:]]" | awk '!seen[$0]++' | \
 sort-bed  --max-mem 8G \
---tmpdir /scratch/aob2x/tmp - > /scratch/aob2x/dest/dest/dgn_drosRTEC_drosEU.sites.noMerge.dm6.bed
+--tmpdir /scratch/aob2x/tmp - > /scratch/aob2x/dest/dest/dgn_drosRTEC_drosEU.sites.noMerge.noDups.dm6.bed
