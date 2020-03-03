8958e3750745aff6bb7f4e0e073aa2a673e31522
Alan Bergland <aob2x@virginia.edu>
2020-03-03 13:05:32 +0100

    Update check_fastq_encoding.sh

--- mappingPipeline/scripts/check_fastq_encoding.sh
+++ mappingPipeline/scripts/check_fastq_encoding.sh
@@ -11,4 +11,20 @@
 #SBATCH --account berglandlab
 
 
-gunzip SRX2885350_2.fastq.gz | head awk 'NR % 4 == 0' | guess-encoding.py -n 1000
+wd=/scratch/aob2x/dest
+
+chmod +x ${wd}/DEST/mappingPipeline/scripts/guess_encoding.py
+
+if test -f ${wd}/fastq/qualEncodings.delim; then
+    rm ${wd}/fastq/qualEncodings.delim
+    touch ${wd}/fastq/qualEncodings.delim
+else
+    touch ${wd}/fastq/qualEncodings.delim
+fi
+
+for f in ${wd}/fastq/*fastq.gz; do
+  ### f=/scratch/aob2x/dest/fastq/SRX2885350_2.fastq.gz
+  zcat $f | head -n 100 | awk 'NR % 4 == 0' | \
+  ${wd}/DEST/mappingPipeline/scripts/guess_encoding.py | grep -v "#" |
+  sed -E "s|^|${f}\t|g" >> ${wd}/fastq/qualEncodings.delim
+done
