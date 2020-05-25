b6f667fbb1b7f95e79a00433ffec797907e28790
Alan Bergland <aob2x@virginia.edu>
2020-05-25 07:37:04 -0400

    Update concatenate.sh

--- add_DGN_data/concatenate.sh
+++ add_DGN_data/concatenate.sh
@@ -3,7 +3,7 @@
 #SBATCH -J download_DGN # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 6:00:00 ### 6 hours
+#SBATCH -t 1:00:00 ### 6 hours
 #SBATCH --mem 1G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/concatenate.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/concatenate.%A_%a.err # Standard error
