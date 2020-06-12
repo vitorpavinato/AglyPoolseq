078ffa19d8cf7cbffdcea0677739615dc529158a
Alan Bergland <aob2x@virginia.edu>
2020-06-12 12:41:11 -0400

    updates to vcf format

--- snpCalling/PoolSnp.py
+++ snpCalling/PoolSnp.py
@@ -110,9 +110,9 @@ print("""##INFO=<ID=ADP,Number=1,Type=Integer,Description=\"Average per-sample d
 ##INFO=<ID=NC,Number=1,Type=Integer,Description=\"Number of samples not called\">
 ##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">
 ##FORMAT=<ID=RD,Number=1,Type=Integer,Description=\"Reference Counts\">
-##FORMAT=<ID=AD,Number=1,Type=Integer,Description=\"Alternative Counts\">
+##FORMAT=<ID=AD,Number=A,Type=Integer,Description=\"Alternative Counts\">
 ##FORMAT=<ID=DP,Number=1,Type=Integer,Description=\"Total Depth\">
-##FORMAT=<ID=FREQ,Number=1,Type=Float,Description=\"Variant allele frequency\">""")
+##FORMAT=<ID=FREQ,Number=A,Type=Float,Description=\"Variant allele frequency\">""")
 print("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t"+"\t".join(options.n.split(",")))
 
 for l in load_data(data):
---
+++ snpCalling/gather_poolsnp.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J split_and_run # A single job name for the array
+#SBATCH --ntasks-per-node=1 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 1:00:00 ### 6 hours
+#SBATCH --mem 1G
+#SBATCH -o /scratch/aob2x/dest/slurmOutput/split_and_run.%A_%a.out # Standard output
+#SBATCH -e /scratch/aob2x/dest/slurmOutput/split_and_run.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+### run as: sbatch --array=1-$( wc -l ${wd}/dest/poolSNP_jobs.csv | cut -f1 -d' ' ) ${wd}/DEST/PoolSNP4Sync/run_poolsnp.sh
+### sbatch --array=1-5 ${wd}/DEST/PoolSNP4Sync/run_poolsnp.sh
+### sacct -j 12755543
+module load htslib bcftools
+
+wd="/scratch/aob2x/dest"
+outdir="/scratch/aob2x/dest/sub_vcfs"
+
+head -n5 ${wd}/dest/poolSNP_jobs.csv | sed 's/,/_/g' | sed 's/^/\/scratch\/aob2x\/dest\/sub_vcfs\//g' | sed 's/$/.bcf/g' > /scratch/aob2x/dest/sub_vcfs/bcfs_order
+
+bcftools concat \
+-f /scratch/aob2x/dest/sub_vcfs/bcfs_order \
+-O b \
+-o ~/test.all.bcf
+
+bcftools view ~/test.all.bcf | less -S
