7e5092c71188e8121d2b4af074f81356395dae3e
Alan Bergland <aob2x@virginia.edu>
2020-06-14 07:23:34 -0400

    Update runTouchup.sh

--- misc_obsolete/runTouchup.sh
+++ misc_obsolete/runTouchup.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 #
 #SBATCH -J touchup # A single job name for the array
-#SBATCH --ntasks-per-node=10 # one core
-#SBATCH -N 1 # on one node
 #SBATCH -t 48:00:00 ### 6 hours
-#SBATCH --mem 30G
+#SBATCH --cpus-per-task=16
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/touchup.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/touchup.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
+#SLURM_ARRAY_TASK_ID=1
+
+
 wd=/scratch/aob2x/dest
 ### run as: sbatch --array=1-$( tail -n1 /scratch/aob2x/fastq/todl.csv | cut -f3 -d',' ) ${wd}/DEST/misc_obsolete/runTouchup.sh
-
+### sacct -j 12768256
 module load singularity
 
 #SLURM_ARRAY_TASK_ID=1
@@ -22,13 +23,13 @@ sampName=$( grep ",${SLURM_ARRAY_TASK_ID}$" /scratch/aob2x/fastq/todl.csv | cut 
 
 echo $sampName " / " $sranum
 
-
 cd /scratch/aob2x/test
 
 singularity run \
 /scratch/aob2x/dest/dmelsync_hpc.sif \
 /scratch/aob2x/fastq/${sranum}_1.fastq.gz \
 /scratch/aob2x/fastq/${sranum}_1.fastq.gz \
-${sampName}_sub \
 ${sampName} \
--c 20
+${sampName} \
+-c $SLURM_CPUS_PER_TASK \
+-dps
