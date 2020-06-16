a0fa21a16087a3b238a925be3956630f6b46fd89
Alan Bergland <aob2x@virginia.edu>
2020-06-16 10:49:14 -0400

    Update annotate.sh

--- snpCalling/annotate.sh
+++ snpCalling/annotate.sh
@@ -16,18 +16,18 @@ wd=/scratch/aob2x/dest
 
 
 bcftools concat \
-${wd}/sub_bcfs/dest.June14_2020.${maf}.2L.bcf \
-${wd}/sub_bcfs/dest.June14_2020.${maf}.2R.bcf \
-${wd}/sub_bcfs/dest.June14_2020.${maf}.3L.bcf \
-${wd}/sub_bcfs/dest.June14_2020.${maf}.3R.bcf \
-${wd}/sub_bcfs/dest.June14_2020.${maf}.X.bcf \
-${wd}/sub_bcfs/dest.June14_2020.${maf}.4.bcf \
-${wd}/sub_bcfs/dest.June14_2020.${maf}.Y.bcf \
+${wd}/sub_bcf/dest.June14_2020.${maf}.2L.bcf \
+${wd}/sub_bcf/dest.June14_2020.${maf}.2R.bcf \
+${wd}/sub_bcf/dest.June14_2020.${maf}.3L.bcf \
+${wd}/sub_bcf/dest.June14_2020.${maf}.3R.bcf \
+${wd}/sub_bcf/dest.June14_2020.${maf}.X.bcf \
+${wd}/sub_bcf/dest.June14_2020.${maf}.4.bcf \
+${wd}/sub_bcf/dest.June14_2020.${maf}.Y.bcf \
 -n \
 -o ${wd}/dest.June14_2020.${maf}.bcf
 
 bcftools view ${wd}/dest.June14_2020.${maf}.bcf > ${wd}/dest.June14_2020.${maf}.vcf
 
-java -jar ~/snpEff/snpEff.jar eff BDGP6.86 ${wd}/dest.June14_2020.${maf}.vcf -v > ${wd}/dest.June14_2020.${maf}.ann.vcf
+java -jar ~/snpEff/snpEff.jar eff BDGP6.86 ${wd}/dest.June14_2020.${maf}.vcf -v | bgzip -c - > ${wd}/dest.June14_2020.${maf}.ann.vcf
 
 #Rscript --vanilla ${wd}/DEST/snpCalling/vcf2gds.R
