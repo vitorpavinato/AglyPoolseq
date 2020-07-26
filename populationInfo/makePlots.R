fe2ad96d139857dadec51be3e93b3462462acb9a
Alan Bergland <aob2x@virginia.edu>
2020-07-25 22:59:35 -0400

    debug

--- populationInfo/makePlots.R
+++ populationInfo/makePlots.R
@@ -51,6 +51,7 @@
 		scale_x_date(date_labels = "%b", limits = as.Date(c(110,355), origin = as.Date("2018-01-01"))) +
 		xlab("Collection Date") + ylab("Latitude")
 
+
 		ggsave(multi_sample, file="./DEST/populationInfo/multiSample.pdf")
 
 
@@ -58,7 +59,7 @@
 
 	world <- as.data.table(map_data("world"))
 
-	samps.ag.ag <- samps.ag[,list(n=sum(nTime), lat=mean(lat), long=mean(long)), list(locality)]
+	samps.ag.ag <- samps.ag[,list(n=sum(nTime), lat=mean(lat), long=mean(long)), list(locality, set)]
 
 ### make maps
 
@@ -69,15 +70,15 @@
 	# [long>=min.long.eu & long<= max.long.eu][lat>=min.lat.eu & lat<=max.lat.eu]
 	#[longitude>=min.long.eu & longitude<= max.long.eu][latitude>=min.lat.eu & latitude<=max.lat.eu]
 
-
-	world <- 	ggplot() +
+	world.plot <- 	ggplot() +
 				geom_polygon(data = world,
 							aes(x=long, y = lat, group = group), fill="lightgrey") +
 				geom_point(data = samps.ag.ag,
-							aes(x=long, y=lat, size=I((n-1)/2 + 4)), alpha=.5) +
+							aes(x=long, y=lat, size=I((n-1)/2 + 4), color=set), alpha=.5) +
 				xlab("Longitude") + ylab("Latitude") + scale_fill_manual(values="black")
 
-	ggsave(world, file="./DEST/populationInfo/worldPlot.pdf", height=4, width=6)
+
+	ggsave(world.plot, file="./DEST/populationInfo/worldPlot.pdf", height=4, width=6)
 
 
 	## north america
--- utils/sequencingSummaryStats.R
+++ utils/sequencingSummaryStats.R
@@ -4,6 +4,8 @@
 ### libraries
   library(SeqArray)
   library(data.table)
+  library(foreach)
+  library(Rsamtools)
 
 ### open GDS file
   gds.fn <- "/scratch/aob2x/dest/dest.July6_2020.001.10.ann.gds"
@@ -16,14 +18,120 @@
                         variant.id=seqGetData(genofile, "variant.id"),
                         nAlleles=seqNumAllele(genofile))
 
-### get depths
-  seqSetFilter(genofile, variant.id=snps.dt[nAlleles==2]$variant.id[1:1000])
+### get read depths
+  nSNPs <- 10000
+  seqSetFilter(genofile, variant.id=as.numeric(sample(as.character(snps.dt[nAlleles==2]$variant.id), nSNPs)))
   #tmp.ad <- seqGetData(genofile, "annotation/format/AD")
   #tmp.rd <- seqGetData(genofile, "annotation/format/RD")
   tmp.dp <- seqGetData(genofile, "annotation/format/DP")
 
   dat <- data.table(dp=expand.grid(tmp.dp$data)$Var1,
-                    population=rep(seqGetData(genofile, "sample.id"), dim(tmp.dp$data)[2]),
+                    sampleId=rep(seqGetData(genofile, "sample.id"), dim(tmp.dp$data)[2]),
                     variant.id=rep(seqGetData(genofile, "variant.id"), each=dim(tmp.dp$data)[1]))
+  dat[,variant.id:=as.numeric(variant.id)]
 
-  dat.ag <- dat[,list(mu=mean(dp, na.rm=T), nmissing=sum(is.na(dp))), list(population)]
+  setkey(snps.dt, variant.id)
+  setkey(dat, variant.id)
+
+  dat <- merge(dat, snps.dt)
+  setkey(dat, chr)
+  dat.ag <- dat[J(c("2L", "2R", "3L", "3R", "X")),
+                  list(mu=mean(dp, na.rm=T), nmissing=sum(is.na(dp))), list(sampleId, auto=!(chr=="X"))]
+
+### incorporate metadata
+  samps <- fread("/scratch/aob2x/dest/DEST/populationInfo/samps.csv")
+
+  m <- merge(dat.ag, samps)
+
+
+### pull in PCR dup rate
+  fns <- system("ls /project/berglandlab/DEST/dest_mapped/*/*/*duplicates_report.txt", intern=T)
+
+  pcrs <- foreach(fn=fns)%do%{
+    #fn <- fns[1]
+    data.table(pcrDup=fread(fn, skip="LIBRARY", nrows=1)$PERCENT_DUPLICATION, sampleId=tstrsplit(fn, "/")[[7]])
+  }
+  pcrs <- rbindlist(pcrs)
+  setkey(pcrs, sampleId)
+
+  mp <- merge(m, pcrs, all.x=T)
+
+### simulans contamination rate
+  simContam <- foreach(samp.i=m[set!="dgn"][auto==T]$sampleId)%do%{
+    #samp.i=m[set!="dgn"][auto==T]$sampleId[1]
+    message(samp.i)
+    simBam <- gsub("mark_duplicates_report.txt", "sim.bam", fns[grepl(samp.i, fns)])
+    simIdx <- paste(simBam, "bai", sep=".")
+
+    melBam <- gsub("mark_duplicates_report.txt", "mel.bam", fns[grepl(samp.i, fns)])
+    melIdx <- paste(melBam, "bai", sep=".")
+
+    simidx.out <- as.data.table(idxstatsBam(file=simBam, index=simIdx))[grepl("2L|2R|3L|3R|X", seqnames)][!grepl("Het|het|Sac|Sca", seqnames)]
+    melidx.out <- as.data.table(idxstatsBam(file=melBam, index=melIdx))[grepl("2L|2R|3L|3R|X", seqnames)][!grepl("Het|het|Sac|Sca", seqnames)]
+
+    idx.out <- merge(melidx.out, simidx.out, by="seqnames")
+
+    data.table(propSim=sum(idx.out$mapped.y)/(sum(idx.out$mapped.y) + sum(idx.out$mapped.x)),
+              sampleId=samp.i)
+
+  }
+  simContam <- rbindlist(simContam)
+  setkey(mp, sampleId)
+  setkey(simContam, sampleId)
+  mps <- merge(mp, simContam, all.x=T)
+
+  save(mps, file="~/mps.Rdata")
+  save(mps, file="/scratch/aob2x/dest/DEST/populationInfo/mps.Rdata")
+
+###
+  # scp aob2x@rivanna.hpc.virginia.edu:~/mps.Rdata ~/.
+
+### plots
+  library(ggplot2)
+  library(data.table)
+  library(cowplot)
+
+### load adata
+  load("~/mps.Rdata")
+
+### a few small fixes
+  mps[continent=="North_America", continent:="NorthAmerica"]
+
+  mps[,effRD:=(mu.r * 2*nFlies) / (mu.r + 2*nFlies)]
+  setnames(mps, "mu", "AveReadDepth")
+
+### rank x-axis to mean read depth
+  mps <- mps[auto==T]
+  mps[,x:=rank(AveReadDepth, ties.method="first")]
+  mps[,x.id:=factor(sampleId, levels=mps$sampleId[mps$mu.r])]
+
+  mps[,propMissing:=nmissing/10000]
+
+### wide to long
+  mpsl <- melt(mps,
+              id.vars=c("x", "sampleId", "continent", "auto", "set"),
+              measure.vars=c("AveReadDepth", "propMissing", "nFlies", "effRD", "pcrDup", "propSim"))
+
+  mpsl[,xf:=as.factor(x)]
+
+### plot
+  summaryStat.plot <- ggplot(data=mpsl, aes(x=xf, y=value, color=continent, fill=continent)) +
+          geom_point(pch=21, alpha=.5, size=2) +
+          facet_grid(variable~set, scales="free", space="free_x") +
+          theme(axis.text.x = element_blank(), panel.spacing = unit(1, "lines"))
+
+  ggsave(summaryStat.plot, file="~/summaryStat_plot.pdf", h=10, w=8)
+
+
+### any collinearity with propMissing? Not really
+  cor(mps[, c("AveReadDepth", "propMissing", "nFlies", "effRD", "pcrDup", "propSim"), with=F], use="complete")
+
+  summary(lm(propMissing ~ propSim * pcrDup * , mps))
+  summary(lm(propSim ~ pcrDup  , mps))
+
+### labeled samples
+    missing <- ggplot(data=mps[auto==T], aes(x=as.factor(mu.r), y=nmissing/10000, color=continent, fill=continent)) +
+    geom_point(pch=21, alpha=.5, size=2) +
+    facet_grid(auto~set, scales="free_x", space="free_x") +
+    theme(axis.text.x = element_blank()) +
+    geom_text(aes(label=ifelse((nmissing/10000)>.1, as.character(sampleId),'')), hjust=0,vjust=0)
