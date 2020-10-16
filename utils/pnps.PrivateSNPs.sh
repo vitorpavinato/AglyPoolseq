d84eff92eea78ceabd0b78f8e6169cd82cfa9e02
Alan Bergland <aob2x@virginia.edu>
2020-10-16 12:31:38 -0400

    Update pnps.PrivateSNPs.sh

--- utils/pnps.PrivateSNPs.sh
+++ utils/pnps.PrivateSNPs.sh
@@ -4,10 +4,10 @@
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
 #SBATCH -t 4:00:00 ### 6 hours
-#SBATCH --mem 20G
+#SBATCH --mem 64G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/pnpsPrivate.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/pnpsPrivate.%A_%a.err # Standard error
-#SBATCH -p standard
+#SBATCH -p largemem
 #SBATCH --account berglandlab
 
 
@@ -17,6 +17,11 @@
 
 ### run as: sbatch --array=1 ${wd}/DEST/utils/pnps.PrivateSNPs.sh
 ### sacct -j 17164717
+
+#sacct -j 17166145 | grep "OUT_OF" | grep -v "batch" | cut -f1 -d' ' | cut -f2 -d'_' | tr '\n' ','
+
+### sbatch --array=3,4,7,28,75,80,81,102,114,141,151,152,157,167,168,185,186,191,212,230,232 ${wd}/DEST/utils/pnps.PrivateSNPs.sh
+### sacct -j 17187383
 
 module load intel/18.0 intelmpi/18.0 R/3.6.3
 
