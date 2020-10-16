caaadf3d4d16bea9e3e9c643cb1b78ff62f6ffcd
Alan Bergland <aob2x@virginia.edu>
2020-10-16 14:56:21 -0400

    Update pnps.PrivateSNPs.sh

--- utils/pnps.PrivateSNPs.sh
+++ utils/pnps.PrivateSNPs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #
 #SBATCH -J pnpsPrivate # A single job name for the array
-#SBATCH --ntasks-per-node=1 # one core
+#SBATCH --ntasks-per-node=3 # one core
 #SBATCH -N 1 # on one node
 #SBATCH -t 4:00:00 ### 6 hours
-#SBATCH --mem 60G
+#SBATCH --mem 180G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/pnpsPrivate.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/pnpsPrivate.%A_%a.err # Standard error
 #SBATCH -p largemem
@@ -21,7 +21,10 @@
 #sacct -j 17166145 | grep "OUT_OF" | grep -v "batch" | cut -f1 -d' ' | cut -f2 -d'_' | tr '\n' ','
 
 ### sbatch --array=3,4,7,28,75,80,81,102,114,141,151,152,157,167,168,185,186,191,212,230,232 ${wd}/DEST/utils/pnps.PrivateSNPs.sh
-### sacct -j 17187383
+### sacct -j 17196415
+
+### sbatch --array=102,114,151 ${wd}/DEST/utils/pnps.PrivateSNPs.sh
+### sacct -j 17196415
 
 module load intel/18.0 intelmpi/18.0 R/3.6.3
 
