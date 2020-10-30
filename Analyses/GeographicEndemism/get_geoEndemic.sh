40fa97208b925167fba166a51d1dd4cc2a0a74b7
Alan Bergland <aob2x@virginia.edu>
2020-10-29 20:08:58 -0400

    Update get_geoEndemic.sh

--- Analyses/GeographicEndemism/get_geoEndemic.sh
+++ Analyses/GeographicEndemism/get_geoEndemic.sh
@@ -3,7 +3,7 @@
 #SBATCH -J lme4qtl # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
-#SBATCH --cpus-per-task=10 ### standard has 28 or 40 $SLURM_CPUS_PER_TASK
+#SBATCH --cpus-per-task=1 ### standard has 28 or 40 $SLURM_CPUS_PER_TASK
 #SBATCH -t 7:00:00 # Running time of 1 hours
 #SBATCH --mem 1G # Memory request of 8 GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/lme4qtl.%A_%a.out # Standard output
@@ -11,8 +11,8 @@
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
-# sbatch /scratch/aob2x/dest/DEST/utils/get_geoEndemic.sh
-# sacct -j
+# sbatch /scratch/aob2x/dest/DEST/Analyses/GeographicEndemism/get_geoEndemic.sh
+# sacct -j 18137921
 
 
 zcat /scratch/aob2x/dest/dest.PoolSeq.SNAPE.001.50.ann.vcf.gz | grep -v "#" | awk '{
