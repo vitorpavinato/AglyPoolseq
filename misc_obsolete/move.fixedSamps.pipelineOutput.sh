8a1f03c93c51a1d56722ac28610485fdc48a9839
Alan Bergland <aob2x@virginia.edu>
2020-11-10 07:40:42 -0500

    Update move.fixedSamps.pipelineOutput.sh

--- misc_obsolete/move.fixedSamps.pipelineOutput.sh
+++ misc_obsolete/move.fixedSamps.pipelineOutput.sh
@@ -12,21 +12,23 @@
 
 
 # sbatch /scratch/aob2x/dest/DEST/misc_obsolete/move.fixedSamps.pipelineOutput.sh
+# sacct -j 18771063
 
-ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/AT_gr_12_fall
-ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/AT_gr_12_spring
-ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/ES_ba_12_fall
-ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/ES_ba_12_spring
+
+#ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/AT_gr_12_fall
+#ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/AT_gr_12_spring
+#ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/ES_ba_12_fall
+#ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/ES_ba_12_spring
 
 rm -fr /project/berglandlab/DEST/dest_mapped/pipeline_output/AT_gr_12_fall
 rm -fr /project/berglandlab/DEST/dest_mapped/pipeline_output/AT_gr_12_spring
 rm -fr /project/berglandlab/DEST/dest_mapped/pipeline_output/ES_ba_12_fall
 rm -fr /project/berglandlab/DEST/dest_mapped/pipeline_output/ES_ba_12_spring
 
-rsync /scratch/aob2x/tempOut/AT_gr_12_fall /project/berglandlab/DEST/dest_mapped/pipeline_output/.
-rsync /scratch/aob2x/tempOut/AT_gr_12_spring /project/berglandlab/DEST/dest_mapped/pipeline_output/.
-rsync /scratch/aob2x/tempOut/ES_ba_12_fall /project/berglandlab/DEST/dest_mapped/pipeline_output/.
-rsync /scratch/aob2x/tempOut/ES_ba_12_spring /project/berglandlab/DEST/dest_mapped/pipeline_output/.
+rsync -a /scratch/aob2x/tempOut/AT_gr_12_fall /project/berglandlab/DEST/dest_mapped/pipeline_output/.
+rsync -a /scratch/aob2x/tempOut/AT_gr_12_spring /project/berglandlab/DEST/dest_mapped/pipeline_output/.
+rsync -a /scratch/aob2x/tempOut/ES_ba_12_fall /project/berglandlab/DEST/dest_mapped/pipeline_output/.
+rsync -a /scratch/aob2x/tempOut/ES_ba_12_spring /project/berglandlab/DEST/dest_mapped/pipeline_output/.
 
 ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/AT_gr_12_fall
 ls -lh /project/berglandlab/DEST/dest_mapped/pipeline_output/AT_gr_12_spring
