6a45db36e1d77425cc707e242e7840f525c18d81
Joaquin Nunez <jcbnunez@JCBN2020.local>
2020-12-30 00:02:39 -0500

    Extended Endemism

---
+++ Analyses/GeographicEndemism/Extended_endemism/run_SNA_all_0.05.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem=60G
+#SBATCH --time=8:00:00
+#SBATCH --partition=standard
+#SBATCH --account=jcbnunez
+
+module load gcc/7.1.0  
+module load openmpi/3.1.4
+module load gdal
+module load proj
+module load htslib/1.9
+module load bcftools/1.9
+module load intel/18.0  
+module load intelmpi/18.0
+module load R/4.0.0
+
+Rscript 2.1.AlleleFreq.SNAPE.Allsamps.0.05.R
