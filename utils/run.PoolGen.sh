fda5a6d9a4594dcbf9b71d8e2cf6a0a4511d1425
Alan Bergland <aob2x@virginia.edu>
2021-01-05 15:00:19 -0500

    Update run.PoolGen.sh

--- utils/run.PoolGen.sh
+++ utils/run.PoolGen.sh
@@ -4,15 +4,15 @@
 #SBATCH --ntasks-per-node=5 # one core
 #SBATCH -N 1 # on one node
 #SBATCH -t 2:00:00 ### 1 hours
-#SBATCH --mem 60G
+#SBATCH --mem 150G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/poolgen.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/poolgen.%A_%a.err # Standard error
-#SBATCH -p standard
+#SBATCH -p largemem
 #SBATCH --account berglandlab
 
 
 ### run as: sbatch --array=1-246 ${wd}/DEST/utils/run.PoolGen.sh
-### sacct -j 19358791
+### sacct -j 19359503
 ### cat /scratch/aob2x/dest/slurmOutput/poolgen.19358567_1.out
 
 ### cat /scratch/aob2x/dest/slurmOutput/poolgen.19358314_1.out
