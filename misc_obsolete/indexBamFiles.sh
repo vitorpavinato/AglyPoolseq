24768e8f9b66ee12b0c9e6b8376d72f97509eb8c
Alan Bergland <aob2x@virginia.edu>
2021-01-06 08:01:25 -0500

    Update indexBamFiles.sh

--- misc_obsolete/indexBamFiles.sh
+++ misc_obsolete/indexBamFiles.sh
@@ -3,7 +3,7 @@
 #SBATCH -J move # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 1:00:00 ### 0.5 hours
+#SBATCH -t 0:30:00 ### 0.5 hours
 #SBATCH --mem 1G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/index.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/index.%A_%a.err # Standard error
