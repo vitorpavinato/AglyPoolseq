9cf7be74f79eca59413fba7c1997b68870daa36e
Alan Bergland <aob2x@virginia.edu>
2021-01-15 12:49:41 -0500

    Update reheader_vcf_bcf.sh

--- utils/fix_Ukraine_sampleNames/reheader_vcf_bcf.sh
+++ utils/fix_Ukraine_sampleNames/reheader_vcf_bcf.sh
@@ -3,12 +3,16 @@
 #SBATCH -J reheader # A single job name for the array
 #SBATCH --ntasks-per-node=9 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 1:00:00 ### 6 hours
+#SBATCH -t 6:00:00 ### 6 hours
 #SBATCH --mem 10G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/reheader.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/reheader.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
+
+### sbatch /scratch/aob2x/dest/DEST/utils/fix_Ukraine_sampleNames/reheader_vcf_bcf.sh
+### sacct -j 19602109
+
 
 module load bcftools parallel samtools
 
