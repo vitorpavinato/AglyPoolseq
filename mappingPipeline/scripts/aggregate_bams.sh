4691dda501f695581f53780e0a620d89d569cc11
Vitor Pavinato <vitor.pavi@gmail.com>
2021-04-21 14:17:28 -0400

    script to aggregate bams

---
+++ mappingPipeline/scripts/aggregate_bams.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+check_exit_status () {
+  if [ ! "$2" -eq "0" ]; then
+    echo "Step $1 failed with exit status $2"
+    exit $2
+  fi
+  echo "Checked step $1"
+}
+
+suffix="test_file"
+sample="default_sample_name"
+input="."
+output="."
+
+# Credit: https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+    echo "Usage:"
+    echo "  aggregate_bams -h Display this help message."
+    echo "  aggregate_bams <bam_suffix_file> <input_dir> <sample_name> <output_dir>"
+    exit 0
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") #save it to an array
+    shift
+    ;;
+esac
+done
+
+set -- "${POSITIONAL[@]}"
+
+if [ $# != 4 ]
+  then
+    echo "ERROR: Need to supply <bam_suffix_file> <input_dir> <sample_name> <output_dir> as positional arguments, and no others"
+    exit 1
+fi
+
+bam=$1; shift
+input=$1; shift
+sample=$1; shift
+output=$1; shift
+
+if [ ! -d $output/$sample/ ]; then
+  mkdir -p $output/$sample/
+fi
+  
+if [ls $input/${sample}_*/${sample}_*.${suffix}.bam | wc -l == 5]; then
+    echo "There are 5"
+else
+    echo "There are 4"
+fi
+
+check_exit_status "Picard_MergeSamFiles" $?
+  
+  
---
+++ mappingPipeline/scripts/runAggregateBams.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=aggregateBams # A single job name for the array
+##SBATCH --ntasks-per-node=10 # one core
+#SBATCH -c 3
+#SBATCH -N 1 # on one node
+#SBATCH -t 00:30:00
+#SBATCH --mem 12G
+#SBATCH -o /fs/scratch/PAS1715/aphidpool/slurmOutput/aggregateBams.%A_%a.out # Standard output
+#SBATCH -e /fs/scratch/PAS1715/aphidpool/slurmOutput/aggregateBams.%A_%a.err # Standard error
+#SBATCH --account PAS1715
+
+### modules
+  module load singularity
+
+### define a few things
+  wd=/fs/scratch/PAS1715/aphidpool
+  outputDir=/fs/scratch/PAS1715/aphidpool/dest_mapped/pipeline_output/aggregated
+  inputDir=/fs/scratch/PAS1715/aphidpool/dest_mapped/pipeline_output/
+
+### get job number
+  pool=$( cat ${wd}/DEST-AglyPoolseq/populationInfo/fieldPools_aggregated.csv | cut -f1,13 -d',' | grep -v "NA" | sed "${SLURM_ARRAY_TASK_ID}q;d" | cut -f1 -d',' )
+  
+  echo $pool
+  
+  touch /fs/scratch/PAS1715/aphidpool/dest_mapped/pipeline_output/${pool}_*/${pool}_*.sorted_merged.bam
+  
+### run aggregateBams.sh
+  
+  singularity exec aglypoolseq_latest.sif \
+  sh /opt/DEST-AglyPoolseq/mappingPipeline/scripts/aggregateBams.sh \
+  sorted_merged \
+  ${inputDir} \
+  ${pool} \
+  ${outputDir}
