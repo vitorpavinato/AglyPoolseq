febe23f7a3f6719f41cbd88365a8ee80f10b91b6
Vitor Pavinato <vitor.pavi@gmail.com>
2021-06-28 16:03:45 -0400

    aggregated baypass file

--- Analyses/aggregated_data/run_baypass.sh
+++ Analyses/aggregated_data/run_baypass.sh
@@ -3,7 +3,7 @@
 ##SBATCH --ntasks-per-node=1 # one core
 #SBATCH -c 5
 #SBATCH -N 1 # on one node
-#SBATCH -t 20:00:00 ### most jobs should run in 60 minutes or less; the mitochondria takes a lot longer to run through pool-snp
+#SBATCH -t 25:00:00 ### most jobs should run in 60 minutes or less; the mitochondria takes a lot longer to run through pool-snp
 #SBATCH --mem 10G
 #SBATCH -o /fs/scratch/PAS1715/aphidpool/slurmOutput/run_baypass.%A_%a.out # Standard output
 #SBATCH -e /fs/scratch/PAS1715/aphidpool/slurmOutput/run_baypass.%A_%a.err # Standard error
@@ -17,16 +17,15 @@ method=${2}
 maf=${3}
 mac=${4}
 version=${5}
-missing=${6}
-pools=${7}
-#maf=001; mac=100; popSet="all"; method="PoolSNP"; version="paramTest"; missing=05; pools="allpools"
+dataset=${6}
+#maf=001; mac=100; popSet="all"; method="PoolSNP"; version="paramTest"; dataset="reduced30"
 
 wd="/fs/scratch/PAS1715/aphidpool"
 
 # Run BAYPASS
 echo "running baypass"
-baypass -gfile ${wd}/data/aggregated_data/genobaypass/aphidpool.${popSet}.${method}.${maf}.${mac}.${version}.vcf.missfrac${missing}.${pools}.genobaypass \
-        -poolsizefile ${wd}/data/aggregated_data/genobaypass/aphidpool.${popSet}.${method}.${maf}.${mac}.${version}.vcf.missfrac${missing}.${pools}.poolsize \
-        -outprefix core.${popSet}.${method}.${maf}.${mac}.${version}.missfrac${missing}.${pools} -nthreads 5 -pilotlength 500 -burnin 5000 -seed 5001;
+baypass -gfile ${wd}/result/aggregated_data/minmaxcov_4_99/baypass_poolsnp/aphidpool.${popSet}.${method}.${maf}.${mac}.${version}.${dataset}.genobaypass \
+        -poolsizefile ${wd}/result/aggregated_data/minmaxcov_4_99/baypass_poolsnp/aphidpool.${popSet}.${method}.${maf}.${mac}.${version}.${dataset}.poolsize \
+        -outprefix core.${popSet}.${method}.${maf}.${mac}.${version}.${dataset} -nthreads 5 -npilot 25 -pilotlength 1000 -burnin 5000 -seed 5001;
 
 echo "done"
